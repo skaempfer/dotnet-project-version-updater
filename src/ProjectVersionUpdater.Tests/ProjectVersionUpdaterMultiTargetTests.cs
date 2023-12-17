fe444f4382bee4b017d9dafd54e8fe6aa863a8be
Sebastian Kämpfer <kontakt@skaempfer.com>
2023-12-17 14:57:46 +0100

    Add test for coping with multi target projects

---
+++ src/ProjectVersionUpdater.Tests/ProjectVersionUpdaterMultiTargetTests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.Build.Locator;
+
+using Xunit;
+
+namespace ProjectVersionUpdater.Tests
+{
+    public class ProjectVersionUpdaterMultiTargetTests
+    {
+        static ProjectVersionUpdaterMultiTargetTests()
+        {
+            VisualStudioInstance net6Instance = MSBuildLocator.QueryVisualStudioInstances().First(i => i.Version.Major == 6);
+            if (!MSBuildLocator.IsRegistered)
+            {
+                MSBuildLocator.RegisterInstance(net6Instance);
+            }
+        }
+
+        [Fact]
+        public async Task IncreaseVersion_MultiTargetProject_IncreasesVersion()
+        {
+            using Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace workspace = Microsoft.CodeAnalysis.MSBuild.MSBuildWorkspace.Create();
+            Microsoft.CodeAnalysis.Solution solution = await workspace.OpenSolutionAsync(Path.GetFullPath("./MultiTargetTestData/MultiTargetTest.sln"));
+            IPrereleaseScheme prereleaseScheme = new CustomPrereleaseScheme("test");
+            ProjectVersionUpdaterFactory updaterFactory = new ProjectVersionUpdaterFactory(solution, prereleaseScheme);
+            
+            ProjectVersionUpdater updater = updaterFactory.Create(Path.GetFullPath("./MultiTargetTestData/A.csproj"));
+
+            updater.IncreaseVersion(VersionPart.Major, prerelease: false);
+        }
+    }
+}
